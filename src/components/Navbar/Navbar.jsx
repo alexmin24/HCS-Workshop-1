d9d147a48d610a013cafa371780fd2476443a5db
Alexandra Min <alexandramin@dhcp-10-250-210-70.harvard.edu>
2020-10-06 20:30:44 -0400

    Committing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import "./Navbar.css";
+import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <>
       <div className="info">
-        <h1 style={{ margin: 0 }}>your name</h1>
+        <h1 style={{ margin: 0 }}>Alexandra Min</h1>
         <div className="info-bar">
-          <p className="info-item">yourname@college.harvard.edu</p>
+          <p className="info-item">alexmin@college.harvard.edu</p>
         </div>
         <div className="info-bar">
-          links to cool things you have online maybe?
+          <Link 
+            classname={"info-link"}
+            to = {{pathname: 'https://instagram.com'}}
+            target = "_blank"
+          >
+            Instagram
+          </Link>
+
+          <Link 
+            classname={"info-link"}
+            to={{pathname: 'https://spotify.com'}}
+            target = "_blank"
+          >
+            Spotify
+          </Link>
+          
+          <Link 
+            classname={"info-link"}
+            to={{pathname: 'https://google.com'}}
+            target = "_blank"
+          >
+            Google
+          </Link>
         </div>
       </div>
-      <div className="nav-background">we need functioning links here!</div>
+      <div className="nav-background">
+        <Link to = {{pathname: "/about"}}>About</Link>
+        <Link to = {{pathname: "/experience"}}>Experience</Link>
+        <Link to = {{pathname: "/projects"}}>Projects</Link>
+        <Link to = {{pathname: "/education"}}>Education</Link>
+      </div>
     </>
   );
 }
--- src/views/Education/Education.jsx
+++ src/views/Education/Education.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./About.css";
+import "./Education.css";
 
-export default function AboutScreen() {
+export default function EducationScreen() {
   return (
     <div className="screen-background">
       <div className="intro-background">
         <div className="intro-item">
-          <div>here you'll put a picture of yourself</div>
+          <div>NOT THE ABOUT PAGE</div>
         </div>
         <div className="intro-item">
           Wow wow wow Your Name is a pretty cool person! check out all these
